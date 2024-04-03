136c637525300a39516fb23a436b3abe4eee8be1
codeofduty-dev <partners@codeofduty.dev>
2024-04-04 00:03:14 +0200

    :sparkles: Feat: Homepage Menu
    MEP Homepage

---
+++ assets/react/controllers/Header/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = () => {
+    return(
+        <nav>
+            <ul>
+                <li><a href="/">Homepage</a></li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Menu
